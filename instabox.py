0f7b5718b7f4a0f719e60ba1635c0dfa4307b40a
biteworks <twitter@tobiaswilhelm.info>
2017-02-26 17:59:06 +0100

    Add files via upload

---
+++ instabox.py
@@ -0,0 +1,91 @@
+#Anzeige des Bildes nach der Aufnahme funktioniert noch nicht
+#Loop bei Bildaufnahme inklusive Anzeige der Nummer
+#Startbildschirm
+#Druckoption??
+
+import random, pygame, sys, os
+from pygame.locals import *
+import picamera
+import time
+import ConfigParser
+
+# Frames und Aufloesung
+FPS = 25
+WINDOWWIDTH = 800
+WINDOWHEIGHT = 480
+EVENTNAME = 'wilhelm'
+IMAGEFOLDER = '/home/pi/Desktop/InstaBox-Images/'
+
+cam = picamera.PiCamera()
+
+# Kamera-Vorschau starten
+def startCam():
+    cam.start_preview(fullscreen=False,window=(0,0,800,480))
+    cam.vflip = True
+    cam.hflip = True
+
+# Kamera-Vorschau beenden
+def endCam():
+    cam.stop_preview()
+
+# Bild aufnehmen in maximaler Aufloesung
+def captureImg(imgName):
+    cam.exposure_mode = 'sports'
+    cam.resolution = (2592, 1944)
+    cam.capture(IMAGEFOLDER + imgName + '.jpg');
+    
+# Bild anzeigen
+def displayImg(imgName):
+    img = pygame.image.load(IMAGEFOLDER + imgName + '.jpg')
+    pygame.display.blit(img, (0, 0))
+    
+# Countdown und Aufnahme
+def captureProcess():
+    startCam()
+    time.sleep(3)
+    endCam()
+
+    # Starte den Aufnahmeprozess
+    #(soll noch in Loop um das Ganze drei mal zu machen)
+    print("3")
+    pygame.time.delay(1000)
+    print("2")
+    pygame.time.delay(1000)
+    print("1")
+    pygame.time.delay(1000)  
+    print("Bitte lachen!")
+    tempTime = time.strftime('%Y-%m-%d_%H-%M-%S')
+    tempImgName = tempTime + '_' + EVENTNAME
+    print (tempImgName)
+
+    # Bild aufnehmen
+    captureImg(tempImgName)
+    pygame.time.delay(1000)
+
+    #Bild anzeigen
+    #displayImg(tempImgName)
+    
+
+def main():
+    # Pygame und Kamera initialisieren
+    pygame.init()
+    
+    # Fenster erstellen
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    pygame.display.set_caption('InstaBox')
+    pygame.mouse.set_visible(False)
+
+    while True:
+        # Update des Renderfensters 
+        pygame.display.update()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    captureProcess()
+
+if __name__ == '__main__':
+    main()

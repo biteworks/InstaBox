6b79197ec85ee28d6176edc02c29e76a9d71299f
biteworks <twitter@tobiaswilhelm.info>
2018-09-05 08:50:28 +0200

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -2,7 +2,9 @@ import time, threading
 
 counter = 10
 countDownRuns = False
+runFunc = "shoot"
 
+#Countdown Funktion
 def countDown():
     global countDownRuns
     global counter
@@ -11,23 +13,38 @@ def countDown():
     counter = counter - 1
 
     if(counter>=1):
-        startTimer()
+        startTimer(runFunc)
         return
+		
+    if(runFunc=="shoot"):	
+        print("shoot")
 
+    elif(runFunc=="run"):
+		print("run")
+		
     countDownRuns = False
 
-def startTimer():
+#Funktion um den Timer zu starten
+def startTimer(runFunc):
     global countDownRuns
 
     countDownRuns = True
 
     t = threading.Timer(1.0, countDown)
-    t.start()  # after 30 seconds, "hello, world" will be printed
+    t.start()
 
+#Funktion um den Timer zu setzen (in Sekunden)
 def setTimer(seconds):
     global counter
     counter = seconds
 
+#Funktion um zu setzen welche Funktion am Ende des Timers aufgerufen wird
+#Bisher gibt die Countdown-Funktion nach Ablauf der Zeit nur eine Konsolenausgabe aus
+def setRunFunc(FuncToRun):
+    global runFunc
+    runFunc = FuncToRun
+
 	
 setTimer(5)
-startTimer()
+setRunFunc("shoot")
+startTimer(runFunc)
